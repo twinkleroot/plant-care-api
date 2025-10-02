07525142608c2c61bdee45b675291a3f3ee4b45a
안정모 <dkswdjah13@naver.com>
2025-10-02 23:07:51 +0900

    rebase

---
+++ src/main/kotlin/com/heeee/plant_care_app/PlantCareAppApplication.kt
@@ -0,0 +1,11 @@
+package com.heeee.plant_care_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlantCareAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlantCareAppApplication>(*args)
+}
